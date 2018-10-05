beefab566d5673e853f5ace20506db4b50c958d5
Viktar Dzmitryieu <Viktar_Dzmitryieu@epam.com>
2018-10-05 15:07:30 +0300

    first commit

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -72,10 +72,7 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-  // const a = x2 - x1;
-  // const b = y2 - y1;
-  // return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-  throw new Error('Not implemented');
+  return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 }
 
 /**
@@ -114,12 +111,11 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  // const compose = x1 * x2 + y1 * y2;
-  // const vectorA = Math.abs(Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2)));
-  // const vectorB = Math.abs(Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2)));
-  // const cosAngle = compose / (vectorA * vectorB);
-  // return Math.acos(cosAngle);
-  throw new Error('Not implemented');
+  const compose = x1 * x2 + y1 * y2;
+  const vectorA = Math.abs(Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2)));
+  const vectorB = Math.abs(Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2)));
+  const cosAngle = compose / (vectorA * vectorB);
+  return Math.acos(cosAngle);
 }
 
 /**
